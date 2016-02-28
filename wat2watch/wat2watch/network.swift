67baf288a4c7c2181202b0bcec2afe2913cb8bb8
Agnivah <agnivahp@gmail.com>
2016-02-27 19:09:26 -0600

    flicks

---
+++ wat2watch/wat2watch/network.swift
@@ -0,0 +1,24 @@
+//
+//  network.swift
+//  wat2watch
+//
+//  Created by Agnivah Poddar on 2/26/16.
+//  Copyright © 2016 Agnivah Poddar. All rights reserved.
+//
+
+import UIKit
+
+class network: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
